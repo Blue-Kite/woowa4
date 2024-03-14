17f9d8ec1ac7c4c025544b17c94fdfff3cb74763
Blue-Kite <bluelemon112@gmail.com>
2024-03-14 14:04:37 +0900

    [docs]: 리드미 수정

---
+++ src/constants/Message.js
@@ -0,0 +1,27 @@
+export const MENU = {
+  appetizer: [
+    { name: "양송이수프", price: 6000 },
+    { name: "타파스", price: 5500 },
+    { name: "시저샐러드", price: 8000 },
+  ],
+  main: [
+    { name: "티본스테이크", price: 55000 },
+    { name: "바비큐립", price: 54000 },
+    { name: "해산물파스타", price: 35000 },
+    { name: "크리스마스파스타", price: 25000 },
+  ],
+  desert: [
+    { name: "초코케이크", price: 15000 },
+    { name: "바비큐립", price: 54000 },
+  ],
+  drink: [
+    { name: "제로콜라", price },
+    { name: "레드와인", price: 60_000 },
+    { name: "샴페인", price: 25_000 },
+  ],
+};
+
+export const ERROR = {
+  DATE: "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.",
+  ORDER: "[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.",
+};
